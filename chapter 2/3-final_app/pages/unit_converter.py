3230c764426bac180be9f21c3a4e82711a83289d
enarroied <eric.narro.ied@gmail.com>
2024-09-08 23:37:08 +0200

    changed order of parameters, unit_converter

--- chapter 2/3-final_app/pages/unit_converter.py
+++ chapter 2/3-final_app/pages/unit_converter.py
@@ -55,15 +55,15 @@ with tgb.Page() as converter_page:  # food_fact_page for food_facts.py
 
     tgb.text("## Volume units", mode="md", class_name="color-secondary header")
     with tgb.layout("1 1 1 1"):
-        tgb.number(label="Cups", value="{cups}", on_change=change_volume)
-        tgb.number(label="Tablespoons", value="{tablespoons}", on_change=change_volume)
-        tgb.number(label="Teaspoons", value="{teaspoons}", on_change=change_volume)
-        tgb.number(label="Millilliters", value="{milliliters}", on_change=change_volume)
+        tgb.number(value="{cups}", label="Cups", on_change=change_volume)
+        tgb.number(value="{tablespoons}", label="Tablespoons", on_change=change_volume)
+        tgb.number(value="{teaspoons}", label="Teaspoons", on_change=change_volume)
+        tgb.number(value="{milliliters}", label="Millilliters", on_change=change_volume)
 
     tgb.text("## Weight units", mode="md", class_name="color-secondary header")
     with tgb.layout("1 1 1 1"):
-        tgb.number(label="Grams", value="{grams}", on_change=change_weight)
-        tgb.number(label="Ounces", value="{ounces}", on_change=change_weight)
+        tgb.number(value="{grams}", label="Grams", on_change=change_weight)
+        tgb.number(value="{ounces}", label="Ounces", on_change=change_weight)
 
         with tgb.layout("1 1"):
             tgb.button(
