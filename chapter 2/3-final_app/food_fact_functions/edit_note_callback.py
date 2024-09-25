964f9d0ee5429f65163afb03e3651718e9e13e5a
Eric Narro <eric.narro@gmail.com>
2024-09-25 20:29:36 +0200

    removed commented code

--- chapter 2/3-final_app/food_fact_functions/edit_note_callback.py
+++ chapter 2/3-final_app/food_fact_functions/edit_note_callback.py
@@ -14,14 +14,3 @@ def edit_note(state, var_name, payload):
         state.df_sales_original = add_note(
             state.df_sales_original, payload["index"], payload["col"], payload["value"]
         )
-
-
-"""
-def edit_note(state, var_name, payload):
-    if payload["col"] == "Note":
-        df_sales_new = state.df_sales.copy()
-        df_sales_new.loc[payload["index"], payload["col"]] = payload["value"]
-        state.df_sales = df_sales_new
-    else:
-        None
-"""
