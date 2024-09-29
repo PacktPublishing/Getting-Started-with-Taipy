241420de5259d0cab14c167c99dd1fb3f89c8bf6
Eric Narro <eric.narro@gmail.com>
2024-09-29 22:08:34 +0200

    added answer files

---
+++ chapter 3/exercices/answer_2.py
@@ -0,0 +1,24 @@
+import taipy as tp
+from taipy import Config, Core, Scope
+
+## Answer 2 ##
+
+## Add a filter using read_kwargs ##
+
+cities_parquet_node_config = Config.configure_parquet_data_node(
+    id="most_populated_cities_parquet",
+    default_path="data/city_data.parquet",
+    engine="pyarrow",  # default
+    compression="gzip",  # default is snappy
+    exposed_type="pandas",
+    read_kwargs={"filters": [("population", ">", 20000000)]},  # ADD THIS
+    scope=Scope.GLOBAL,
+)
+
+core = Core()
+core.run()
+cities_parquet_data_node = tp.create_global_data_node(cities_parquet_node_config)
+
+df_cities = cities_parquet_data_node.read()
+
+print(df_cities.head(10))
