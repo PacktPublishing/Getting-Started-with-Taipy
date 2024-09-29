08f82d161bb5f071f4b95badfd51602174b5e7e1
Eric Narro <eric.narro@gmail.com>
2024-09-29 12:52:45 +0200

    added image data node

---
+++ chapter 3/1-data_nodes_examples/image_data_node.py
@@ -0,0 +1,36 @@
+import rasterio
+import taipy as tp
+from taipy import Config, Core, Scope
+
+
+def read_raster_as_numpy_array(raster_path):
+    """Reads a raster image and returns it as a NumPy array.
+
+    Args:
+        raster_path: The path to the raster image file.
+
+    Returns:
+        A NumPy array representing the raster data.
+    """
+
+    with rasterio.open(raster_path) as src:
+        data = src.read()  # Reads all bands into a 3D NumPy array
+        return data
+
+
+tokyo_image_node_config = Config.configure_generic_data_node(
+    id="tokyo_image",
+    read_fct=read_raster_as_numpy_array,
+    read_fct_args=["../data/tokyo_snow.jpg"],
+    scope=Scope.GLOBAL,
+)
+
+core = Core()
+core.run()
+
+
+tokyo_image_data_node = tp.create_global_data_node(tokyo_image_node_config)
+
+tokyo_image_raster = tokyo_image_data_node.read()
+
+print(tokyo_image_raster)
