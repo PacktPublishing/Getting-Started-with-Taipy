2e009f8dcb084adfa0513aee3164527133857d70
Eric Narro <eric.narro@gmail.com>
2024-09-30 20:34:28 +0200

    fixed Answer 1

--- chapter 3/exercices/answer_1.py
+++ chapter 3/exercices/answer_1.py
@@ -16,7 +16,6 @@ orchestrator.run()
 cities_excel_data_node = tp.create_global_data_node(cities_excel_node_config)
 
 dict_cities = cities_excel_data_node.read()
-
 np_cities = dict_cities["cities"]
 
 print("Data from an Excel file as np array:")
