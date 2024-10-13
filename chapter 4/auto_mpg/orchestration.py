86a5864d01d6a4db17e9357af558f221a7fff949
Eric Narro <eric.narro@gmail.com>
2024-10-13 21:42:54 +0200

    added subscription

--- chapter 4/auto_mpg/orchestration.py
+++ chapter 4/auto_mpg/orchestration.py
@@ -2,9 +2,16 @@ import taipy as tp
 from configuration.config import predict_config, train_auto_pipeline_config
 from taipy import Orchestrator
 
+
+def monitor_scenario(scenario, job):
+    print(f"Running scenario: '{scenario.config_id}' ||| task: '{job.task.config_id}'")
+
+
 orchestrator = Orchestrator()
 orchestrator.run()
 
 
 training_scenario = tp.create_scenario(train_auto_pipeline_config)
 predicting_scenario = tp.create_scenario(predict_config)
+
+tp.subscribe_scenario(monitor_scenario)
