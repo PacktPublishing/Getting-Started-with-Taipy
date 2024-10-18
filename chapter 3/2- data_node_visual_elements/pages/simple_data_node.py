42897f2ac390e29917ed036b9309e0eb312a83d2
Eric Narro <eric.narro@gmail.com>
2024-10-18 23:44:24 +0200

    changed callback

--- chapter 3/2- data_node_visual_elements/pages/simple_data_node.py
+++ chapter 3/2- data_node_visual_elements/pages/simple_data_node.py
@@ -8,12 +8,12 @@ def edit_row(state, var_name, payload):
     df_cities_cp.loc[payload["index"], payload["col"]] = payload["value"]
     state.df_cities = df_cities_cp
 
-    biggest_cities.write(state.df_cities)
+    state.biggest_cities.write(state.df_cities)
 
 
 with tgb.Page() as simple_data_node:
     tgb.text("## Interacting with a Data Node from the GUI", mode="md")
 
     tgb.table(
-        data="{df_cities}", filter=True, editable=True, on_edit=edit_row, rebuild=True
+        data="{df_cities}", filter=True, editable=True, rebuild=True, on_edit=edit_row
     )
