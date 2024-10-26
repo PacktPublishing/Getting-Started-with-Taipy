58deb77794d65a9a55518c383718bdf6424a9fa0
Eric Narro <eric.narro@gmail.com>
2024-10-26 16:54:58 +0200

    added price app

---
+++ chapter 5/prices_app/configuration/config.py
@@ -0,0 +1,39 @@
+import datetime as dt
+
+import pandas as pd
+import taipy as tp
+from algorithms.calculate_price import calculate_price
+from taipy import Config, Frequency, Scope
+
+vat_data_node_config = Config.configure_data_node(
+    id="vat_node", scope=Scope.GLOBAL, default_data=1.2
+)
+
+buying_price_node_config = Config.configure_data_node(
+    id="buying_price_node",
+    scope=Scope.CYCLE,
+)
+
+margin_node_config = Config.configure_data_node(
+    id="margin_node",
+    scope=Scope.SCENARIO,
+)
+
+selling_price_node_config = Config.configure_data_node(
+    id="selling_price_node",
+    scope=Scope.SCENARIO,
+)
+
+
+calculate_price_task_config = Config.configure_task(
+    id="calculate_price_task",
+    function=calculate_price,
+    input=[buying_price_node_config, margin_node_config, vat_data_node_config],
+    output=selling_price_node_config,
+)
+
+scenario_config = Config.configure_scenario(
+    id="pricing_scenario",
+    task_configs=[calculate_price_task_config],
+    frequency=Frequency.MONTHLY,
+)
---
+++ chapter 5/prices_app/pipeline.py
@@ -0,0 +1,81 @@
+import datetime as dt
+
+import pandas as pd
+import taipy as tp
+import taipy.gui.builder as tgb
+from configuration.config import scenario_config
+from taipy import Orchestrator
+
+if __name__ == "__main__":
+
+    Orchestrator().run()
+
+    ##################################
+    ##   Create Scenarios           ##
+    ##################################
+
+    scenario_december_1 = tp.create_scenario(
+        scenario_config,
+        creation_date=dt.datetime(2024, 12, 22),
+        name="Scenario 1 - December",
+    )
+    scenario_december_2 = tp.create_scenario(
+        scenario_config,
+        creation_date=dt.datetime(2024, 12, 25),
+        name="Scenario 2 - December",
+    )
+    scenario_january_1 = tp.create_scenario(
+        scenario_config,
+        creation_date=dt.datetime(2026, 1, 1),
+        name="Scenario 1 - January",
+    )
+    scenario_january_2 = tp.create_scenario(
+        scenario_config,
+        creation_date=dt.datetime(2026, 1, 13),
+        name="Scenario 2 - January",
+    )
+
+    ##################################
+    ##   Write input Data Nodes     ##
+    ##################################
+
+    scenario_december_1.vat_node.write(1.2)  # Not necessry because has default data
+    scenario_december_1.margin_node.write(1.5)
+    scenario_december_1.buying_price_node.write(2)
+
+    scenario_december_1.submit()
+
+    price_december = scenario_december_1.selling_price_node.read()
+    print(f"The price for December with is {round(price_december, 2)}")
+
+    # We didn't submit Scenario december_2, but it shares the Cycle with december_1
+    # The buying_price_node should have the same value as december_1 (they are the same Data Node):
+
+    december_buying_price = scenario_december_2.buying_price_node.read()
+    print(f"The value of december_buying_price's Data Node is: {december_buying_price}")
+
+    # If we try the same with margin_node, we'll have an error, because this Data Node doesn't exist
+    # It doesn't exist because we never submitted scenario_desember_2:
+
+    december_2_margin = scenario_december_2.margin_node.read()
+    print(f"The value of scenario_december_2's margin_node is {december_2_margin}")
+
+    # Also, if we try to access the value of buying_price_node from scenario_january_1 (or _2...)
+    # We will get a None value, since they belong to a different Cycle!
+
+    january_buying_price = scenario_january_2.buying_price_node.read()
+    print(f"The value of january_buying_price's Data Node is: {january_buying_price}")
+
+    ##################################
+    ##   Access Cycle information   ##
+    ##################################
+
+    december_1_cycle = scenario_december_1.cycle
+
+    print(december_1_cycle)
+    print(december_1_cycle.creation_date)
+    print(december_1_cycle.end_date)
+    print(december_1_cycle.start_date)
+    print(december_1_cycle.id)
+    print(december_1_cycle.name)
+    print(december_1_cycle.get_label())
