349c0b9baf0987179f5449646727e8478d9abc8e
Eric Narro <eric.narro@gmail.com>
2024-11-10 21:56:54 +0100

    added linux_app

---
+++ chapter 6/deploy_linux/main.py
@@ -0,0 +1,36 @@
+import taipy as tp
+import taipy.gui.builder as tgb
+from taipy import Config, Gui, Orchestrator
+
+
+def say_hi(person):
+    return f"hi {person}!"
+
+
+node_config_input = Config.configure_data_node(
+    id="node_in",
+    default_data="run method",
+)
+node_config_output = Config.configure_data_node(
+    id="node_out",
+)
+task_config = Config.configure_task(
+    id="task", input=node_config_input, output=node_config_output, function=say_hi
+)
+
+scenario_config = Config.configure_scenario(id="node", task_configs=[task_config])
+
+with tgb.Page() as page:
+    tgb.text("# A Taipy app", mode="md")
+    tgb.text("## {node_message}", mode="md")
+
+
+if __name__ == "__main__":
+    Orchestrator().run()
+    scenario = tp.create_scenario(scenario_config)
+    scenario.submit()
+    node_message = scenario.node_out.read()
+    print(node_message)
+
+    gui = Gui(page=page)
+    app_to_deploy = gui.run(debug=False, run_server=False)
