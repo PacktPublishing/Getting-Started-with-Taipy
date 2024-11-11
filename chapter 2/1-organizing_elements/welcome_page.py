c257ffe1d3b09a12734d997de6f19ec0eadab94b
Eric Narro <eric.narro@gmail.com>
2024-11-11 11:17:22 +0100

    changed image link and added image to repo

--- chapter 2/1-organizing_elements/welcome_page.py
+++ chapter 2/1-organizing_elements/welcome_page.py
@@ -1,6 +1,10 @@
 import taipy.gui.builder as tgb
 
-url_image = "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+# original image from:
+# url_image = "https://images.unsplash.com/photo-1485962398705-ef6a13c41e8f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+url_image = "./images/food.avif"
+
 with open("description.md", "r") as description:
     description_text = description.read()
 
