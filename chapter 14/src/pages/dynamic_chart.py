e6f945d8789f73996e77f53207af56da58e262c3
Eric Narro <eric.narro@gmail.com>
2025-01-30 22:31:21 +0100

    increased size

--- chapter 14/src/pages/dynamic_chart.py
+++ chapter 14/src/pages/dynamic_chart.py
@@ -9,4 +9,4 @@ with tgb.Page() as dynamic_chart_page:
     tgb.table(data="{df_parish_info}")
     tgb.selector("{accommodation}", lov="{accommodation_type}", dropdown=True)
 
-    tgb.part(content="{JsChartClass(df_parish_info, accommodation)}", height="350px")
+    tgb.part(content="{JsChartClass(df_parish_info, accommodation)}", height="500px")
