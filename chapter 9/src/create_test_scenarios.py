58120ae4b5d95c9d4d0a71e9e81cc97ce9dddbbf
enarroied <eric.narro.ied@gmail.com>
2025-04-14 14:50:09 +0200

    removed unnecesary statements 2 - forgot to save...

--- chapter 9/src/create_test_scenarios.py
+++ chapter 9/src/create_test_scenarios.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import taipy as tp
 
 
-def create_and_submit_scenario(id_name, year, scenario_config, df_parks):
+def create_and_submit_scenario(id_name, year, scenario_config):
     scenario_name = f"{id_name} - {year}"
 
     scenario = tp.create_scenario(config=scenario_config, name=scenario_name)
@@ -14,11 +13,10 @@ def create_and_submit_scenario(id_name, year, scenario_config, df_parks):
 
 
 def create_test_scenarios(scenario_config, parks_file="./data/paris_parks.csv"):
-    df_parks = pd.read_csv(parks_file)
     # Bois de Vincennes: 1679
     vincennes = "1679 - Bois De Vincennes"
     for year in range(2023, 2024):
-        create_and_submit_scenario(vincennes, year, scenario_config, df_parks)
+        create_and_submit_scenario(vincennes, year, scenario_config)
 
     # return a specific Scenario for selection
-    return create_and_submit_scenario(vincennes, 2024, scenario_config, df_parks)
+    return create_and_submit_scenario(vincennes, 2024, scenario_config)
