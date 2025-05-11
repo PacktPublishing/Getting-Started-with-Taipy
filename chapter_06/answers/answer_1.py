22dae651a5fab556b7223b92f011d6a35a76fd32
enarroied <eric.narro.ied@gmail.com>
2025-05-11 21:11:49 +0200

    fixed typo in answer 1

--- chapter_06/answers/answer_1.py
+++
@@ -1,8 +0,0 @@
-## This is how the `.run()` method looks:
-
-    gui.run(
-        async_mode="eventlet",
-        time_zone="Indian/Maldives",
-        port="auto",
-        port_auto_ranges=[(1050, 2000)],
-    )
