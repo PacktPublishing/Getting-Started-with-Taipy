6ea1eb659d3b5ab3c027300f673e0eb2fabebff3
enarroied <eric.narro.ied@gmail.com>
2025-05-21 22:39:42 +0200

    added cities app

--- chapter_15/README.md
+++ chapter_15/README.md
@@ -7,3 +7,16 @@ In this chapter, we discover how to use Taipy Designer.
 We start creating a unit converter app, the goal is to discover the basic workflows of Taipy Designer:
 
 ![](img/conver_app.gif)
+
+## Cities App
+
+We create a small demonstration app with world cities. The app displays the top 10 cities for a given country (or "All" countries, or all capitals). The app displays a table, a leaflet map and a matplotlib bar chart. The app also shows two metric cards:
+
+* The total city population for a given selection (the top 10 of a country, capitals, or whole world)
+* And the percentage of total population for that selection
+
+![](img/cities.gif)
+
+### Data
+
+The dataset comes from [Simplemaps](https://simplemaps.com/data/world-cities) (many thanks to them).
---
+++ chapter_15/cities_app/main.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from algorithms.algos import *
+from taipy import Gui
+from taipy.designer import Page
+
+
+def filter_by_country(state):
+    with state as s:
+        df_to_filter = s.df_cities.copy()
+        filter_to_apply = s.selected_country
+
+        if filter_to_apply == "All":
+            df_cities_display = df_to_filter
+        elif filter_to_apply == "Capitals":
+            df_cities_display = s.df_capitals.copy()
+        else:
+            df_to_filter = df_to_filter[
+                df_to_filter.country == filter_to_apply
+            ].reset_index()
+            df_cities_display = df_to_filter
+
+    new_cities_to_display = get_top_10(df_cities_display)
+    with state as s:
+        s.df_cities_display = new_cities_to_display
+        s.top_10_fig = plot_top_10_cities(new_cities_to_display)
+        s.leaflet = make_map(new_cities_to_display)
+        s.selected_cities_pop = new_cities_to_display["population"].sum()
+        s.percent_world_cities = 100 * s.selected_cities_pop / s.total_pop
+
+
+## Callback ##
+def on_change(state, var, val):
+    if var == "selected_country":
+        filter_by_country(state)
+
+
+if __name__ == "__main__":
+    df_cities = clean_arrange_data(pd.read_csv("./data/worldcities.csv"))
+    total_pop = df_cities["population"].sum()
+
+    df_cities_display = df_cities.copy()
+    df_cities_display = get_top_10(df_cities_display)
+    df_capitals = df_cities[df_cities.capital == "primary"].reset_index()
+
+    list_countries = sorted(list(df_cities["country"].unique()))
+    list_country_selection = ["All", "Capitals"] + list_countries
+    selected_country = "All"
+
+    top_10_fig = plot_top_10_cities(df_cities_display)
+    # Leaflet maps need geojsonformat - we use GeoPandas to transform our data
+    leaflet = make_map(df_cities_display)
+    selected_cities_pop = df_cities_display["population"].sum()
+    percent_world_cities = 100 * selected_cities_pop / total_pop
+
+    page = Page("main.xprjson")
+    Gui(page).run(design=True, title="cities app", use_reloader=True)
