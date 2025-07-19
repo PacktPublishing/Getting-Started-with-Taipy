761b9657a6adb158e681afe47c7004b7e54f2b32
enarroied <eric.narro.ied@gmail.com>
2025-07-19 21:02:51 +0200

    fixed typos in instructions

--- chapter_08/src/pages/scenario/scenario.py
+++ chapter_08/src/pages/scenario/scenario.py
@@ -111,13 +111,12 @@ with tgb.Page() as scenario_page:
                 tgb.text(
                     """Select scenario's **parameters** and constraints:
                             
-- **Number of warehouses**: the number of warehouses to select, must be between 1 and 10. If "any", then the applicatioons selects the optimal warehouses with no more than 10.
-- **Fix warehouses**: enable the ability to fix warehouses,
-- **Countries to Include**: If selected, the application will assign **at least** one warehouse to each selected country. The application won't let you select more copuntries than warehouses.
-- **Price per kilometer**: Total cost per kilometer between a warehouse and a customer. Includes all costs (gas, truck maintainance, wages...) for trips back and forth.
-- **CO2 per kilometer**: Total CO2e emissions for transporation of goods between warehouse and customer, for trips back and forth.
-        
-        """,
+- **Number of warehouses**: The number of warehouses to select; must be between 1 and 10. If "any", then the application selects the optimal warehouses with no more than 10.
+- **Fix warehouses**: Enable the ability to fix warehouses.
+- **Countries to include**: If selected, the application will assign **at least** one warehouse to each selected country. The application won't let you select more countries than warehouses.
+- **Price per kilometer**: Total cost per kilometer between a warehouse and a customer. Includes all costs (gas, truck maintenance, wages...) for trips back and forth.
+- **CO2 per kilometer**: Total CO2e emissions for transportation of goods between warehouse and customer, for trips back and forth.
+""",
                     mode="md",
                 )
             with tgb.layout("1 1 1 1 1"):
